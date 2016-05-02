0c26ad89930284843425fe915b24f9ec74a7a4d4
lovesick <blackdancers@163.com>
2016-05-02 23:58:20 +0800

    自动装配之构造器

---
+++ chapter03/src/main/java/com/springinaction/springidol/Poem.java
@@ -0,0 +1,12 @@
+package com.springinaction.springidol;
+
+/**
+ * 诗歌
+ * Created by dujiang on 2016/4/22.
+ */
+public interface Poem {
+    /**
+     * 朗诵，背诵
+     */
+    void recite();
+}
---
+++ springidol/src/main/java/com/springinaction/springidol/Instrument.java
@@ -0,0 +1,9 @@
+package com.springinaction.springidol;
+
+/**
+ * 乐器
+ * Created by dujiang on 2016/4/23.
+ */
+public interface Instrument {
+    void play();
+}
